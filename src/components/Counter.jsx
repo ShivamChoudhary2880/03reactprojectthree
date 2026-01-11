19f8d8d3d9d362b5d8a2927ae0208d40e4edbd13
ShivamChoudhary2880 <shivamchoudhary6033@gmail.com>
2026-01-11 10:02:25 +0530

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './Counter.css';
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0)
+
+    const handleSubmit = () => {
+        setCount(count+1)
+    }
+
+    return (
+        <div className='counter-container'>
+            <p id='para'>You have clicked { count} Times</p>
+            {/* <button onClick={()=> {setCount(count+1)}} id='btn'>Click me</button> */}
+            <button onClick={handleSubmit} id='btn'>Click me</button>
+        </div>
+    )
+}
+
+export default Counter
